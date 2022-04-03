21405dfbc69dff3b2d6d7f7583b2af1f52393ed1
evan.zhang <evan_zhang_zwq@putlook.com>
2022-04-03 15:04:20 +0800

    Split the header tag of website with Django inclusion_tab.

--- blogweb/templatetags/custom_tag.py
+++ blogweb/templatetags/custom_tag.py
@@ -4,7 +4,24 @@ from django import template
 register = template.Library()
 
 
-# Customize a filter with plus functionality
-@register.filter()
-def plus(item):
-    return int(item) + 1
+# Customize a filter with plus functionality, just for testing.
+# @register.filter()
+# def plus(item):
+#     return int(item) + 1
+
+
+@register.inclusion_tag("custom_tag/headers.html")
+def banner(name):
+    """
+    Use "inclusion_tag" to render a header template
+    """
+    print(name)
+
+    header_img_list = [
+        "/static/assets/img/header/g-class1.png",
+        "/static/assets/img/header/g-class2.png",
+        "/static/assets/img/header/g-class3.png",
+        "/static/assets/img/header/g-class4.png"
+    ]
+
+    return {"img_list": header_img_list}
--- blogweb/views.py
+++ blogweb/views.py
@@ -3,10 +3,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    header_img_list = [
-        "/static/assets/img/header/g-class1.png",
-        "/static/assets/img/header/g-class2.png",
-        "/static/assets/img/header/g-class3.png",
-        "/static/assets/img/header/g-class4.png"
-    ]
-    return render(request, 'index.html', {"img_list": header_img_list})
+
+    return render(request, 'index.html')
