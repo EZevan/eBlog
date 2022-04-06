8ddf90dd93c2dea00f831560024b2df7861ddf90
evan.zhang <evan_zhang_zwq@outlook.com>
2022-04-06 15:47:49 +0800

    Prepare the main section ui of page.

--- blogweb/views.py
+++ blogweb/views.py
@@ -3,5 +3,16 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
+    return render(request, "index.html")
 
-    return render(request, 'index.html')
+
+def news(request):
+    return render(request, "news.html")
+
+
+def article(request):
+    return render(request, "article.html")
+
+
+def about(request):
+    return render(request, "about.html")
